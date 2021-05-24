e6f8cad2d1cc400feaae0974766ec0e8f8cd7a46
VadimChm <66498434+VadimChm@users.noreply.github.com>
2021-05-24 20:18:49 +0300

    Update x5.cpp

--- x5.cpp
+++ x5.cpp
@@ -6,9 +6,12 @@ void print (char *m)
   for (int i = 0; i < 5; i++)
     {
       for (int j = 0; j < 5; j++)
-        printf ("%c", m[i * 5 + j]);
+        {
+          printf ("%c", m[i * 5 + j]);
+        }
       printf ("\n");
     }
+  
   printf ("------------\n");
 }
 
@@ -20,7 +23,9 @@ void next_ij (int &i, int &j)
       j = 0;
     }
   else
-    j++;
+    {
+      j++;
+    }
 }
 
 bool can_be_l (int i, int j, char *m)
@@ -28,17 +33,28 @@ bool can_be_l (int i, int j, char *m)
   if (i == 0)
     {
       if (j == 0)
-        return true;
+        {
+          return true;
+        }
       else
-        return (m[j - 1] != 'r');
+        {
+          return (m[j - 1] != 'r');
+        }
     }
   else
     {
       if (j == 0)
-        return (m[(i - 1) * 5] != 'r');
+        {
+          return (m[(i - 1) * 5] != 'r');
+        }
       else
-        return (m[i * 5 + j - 1] != 'r') && (m[(i - 1) * 5 + j] != 'r') && (m[(i - 1) * 5 + j - 1] != 'l');
+        {
+          return (m[i * 5 + j - 1] != 'r') && 
+                 (m[(i - 1) * 5 + j] != 'r') && 
+                 (m[(i - 1) * 5 + j - 1] != 'l');
+        }
     }
+  
   return false;
 }
 
@@ -47,37 +63,59 @@ bool can_be_r (int i, int j, char *m)
   if (i == 0)
     {
       if (j == 0)
-        return true;
+        {
+          return true;
+        }
       else
-        return (m[j - 1] != 'l');
+        {
+          return (m[j - 1] != 'l');
+        }
     }
   else
     {
       if (j == 0)
-        return (m[(i - 1) * 5] != 'l') && (m[(i - 1) * 5 + 1] != 'r');
+        {
+          return (m[(i - 1) * 5] != 'l') && 
+                 (m[(i - 1) * 5 + 1] != 'r');
+        }
       else
         {
           if (j < 4)
-            return (m[i * 5 + j - 1] != 'l') && (m[(i - 1) * 5 + j] != 'l') && (m[(i - 1) * 5 + j + 1] != 'r');
+            {
+              return (m[i * 5 + j - 1] != 'l') && 
+                     (m[(i - 1) * 5 + j] != 'l') && 
+                     (m[(i - 1) * 5 + j + 1] != 'r');
+            }
           else
-            return (m[i * 5 + j - 1] != 'l') && (m[(i - 1) * 5 + j] != 'l');
+            {
+              return (m[i * 5 + j - 1] != 'l') && 
+                     (m[(i - 1) * 5 + j] != 'l');
+            }
         }
     }
+  
   return false;
 }
 
 void find_sol (int i, int j, int res, char *m)
 {
   if (i > 4 || j > 4)
-    return;
-  int ii = i, jj = j;
+    {
+      return;
+    }
+  
+  int ii = i; 
+  int jj = j;
+  
   if (can_be_r (i, j, m))
     {
       m[i * 5 + j] = 'r';
       if (i == 4 && j == 4)
         {
           if (res + 1 == NUM)
-            print (m);
+            {
+              print (m);
+            }
         }
       else
         {
@@ -85,14 +123,19 @@ void find_sol (int i, int j, int res, char *m)
           find_sol (ii, jj, res + 1, m);
         }
     }
-  ii = i; jj = j;
+  
+  ii = i; 
+  jj = j;
+  
   if (can_be_l (i, j, m))
     {
       m[i * 5 + j] = 'l';
       if (i == 4 && j == 4)
         {
           if (res + 1 == NUM)
-            print (m);
+            {
+              print (m);
+            }
         }
       else
         {
@@ -100,13 +143,19 @@ void find_sol (int i, int j, int res, char *m)
           find_sol (ii, jj, res + 1, m);
         }
     }
-  ii = i; jj = j;
+  
+  ii = i; 
+  jj = j;
   m[i * 5 + j] = ' ';
+  
   if (i == 4 && j == 4)
     {
       if (res == NUM)
-        print (m);
+        {
+          print (m);
+        }
     }
+  
   next_ij (ii, jj);
   find_sol (ii, jj, res, m);
   return;
